75dfa956380609a8277cd9f374aefd88d9494605
Michael D'Silva <michael.dsilva@aarnet.edu.au>
2017-07-13 00:21:56 +0000

    moved /cbox/* to /wopi/cbox/*, helps with load balancer. Moved oos server name into config.php rather than hard code into js

--- js/script.js
+++ js/script.js
@@ -13,15 +13,18 @@
 	// the hack for owncloud 8 for having the new file menu entry can work.
 	OCA.WOPIViewer = {};
 
-	var wordViewer = "https://oos.web.cern.ch/wv/wordviewerframe.aspx?WOPISrc=";
-	var wordNew = "https://oos.web.cern.ch/we/wordeditorframe.aspx?new=1&WOPISrc=";
-	var wordEditor = "https://oos.web.cern.ch/we/wordeditorframe.aspx?WOPISrc=";
-	var powerpointViewer = "https://oos.web.cern.ch/p/PowerPointFrame.aspx?WOPISrc=";
-	var powerpointEditor = "https://oos.web.cern.ch/p/PowerPointFrame.aspx?PowerPointView=EditView&WOPISrc=";
-	var powerpointNew = "https://oos.web.cern.ch/p/PowerPointFrame.aspx?PowerPointView=EditView&New=1&WOPISrc=";
-	var excelViewer = "https://oos.web.cern.ch/x/_layouts/xlviewerinternal.aspx?WOPISrc=";
-	var excelNew = "https://oos.web.cern.ch/x/_layouts/xlviewerinternal.aspx?edit=1&new=1&WOPISrc=";
-	var excelEditor = "https://oos.web.cern.ch/x/_layouts/xlviewerinternal.aspx?edit=1&WOPISrc=";
+        var wordViewer = "/wv/wordviewerframe.aspx?WOPISrc=";
+        var wordNew = "/we/wordeditorframe.aspx?new=1&WOPISrc=";
+        var wordEditor = "/we/wordeditorframe.aspx?WOPISrc=";
+        var powerpointViewer = "/p/PowerPointFrame.aspx?WOPISrc=";
+        var powerpointEditor = "/p/PowerPointFrame.aspx?PowerPointView=EditView&WOPISrc=";
+        var powerpointNew = "/p/PowerPointFrame.aspx?PowerPointView=EditView&New=1&WOPISrc=";
+        var excelViewer = "/x/_layouts/xlviewerinternal.aspx?WOPISrc=";
+        var excelNew = "/x/_layouts/xlviewerinternal.aspx?edit=1&new=1&WOPISrc=";
+        var excelEditor = "/x/_layouts/xlviewerinternal.aspx?edit=1&WOPISrc=";
+	var onenoteViewer = "/o/OneNoteFrame.aspx?edit=0&WOPISrc=";
+	var onenoteNew = "/o/OneNoteFrame.aspx?edit=1&new=1&WOPISrc=";
+	var onenoteEditor = "/o/OneNoteFrame.aspx?edit=1&WOPISrc=";
 
 	var closeDocument = function (e) {
 		e.preventDefault();
@@ -95,7 +98,7 @@
 		$.post(url, data).success(function (response) {
 			if (response.wopi_src) {
 				window.location.hash = 'office';
-				var viewerURL = targetURL + encodeURI(response.wopi_src);
+				var viewerURL = response.oos + targetURL + encodeURI(response.wopi_src);
 				setView(viewerURL, response.wopi_src, basename);
 				var closeButton = '<p class="" id="office_close_button" style="display: block; position: absolute; right: 50%; top: 5px"><b>The Office application is in beta</b></p>';
 				$("header div#header").append(closeButton);
@@ -167,22 +170,104 @@
 				sendOpen(filename, data, excelEditor);
 			}
 		},
+		onViewOnenote: function (filename, data) {
+			// if file size is 0 we ask office online
+			// to create an empty docx file
+			var filesize = parseInt(data.$file.attr("data-size"));
+			if(filesize === 0) {
+				sendOpen(filename, data, onenoteNew);
+			} else {
+				sendOpen(filename, data, onenoteViewer);
+			}
+		},
+		onEditOnenote: function (filename, data) {
+			// if file size is 0 we ask office online
+			// to create an empty docx file
+			var filesize = parseInt(data.$file.attr("data-size"));
+			if(filesize === 0) {
+				sendOpen(filename, data, onenoteNew);
+			} else {
+				sendOpen(filename, data, onenoteEditor);
+			}
+		},
 	};
 
 
 	$(document).ready(function () {
 		if (OCA && OCA.Files) {
 			OCA.Files.fileActions.register('application/msword', 'Edit in Office Online', OC.PERMISSION_UPDATE, OC.imagePath('core', 'actions/play'), wopiViewer.onEditWord);
+			OCA.Files.fileActions.register('application/vnd.ms-word.document.macroEnabled.12', 'Edit in Office Online', OC.PERMISSION_UPDATE, OC.imagePath('core', 'actions/play'), wopiViewer.onEditWord);
+			OCA.Files.fileActions.register('application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'Edit in Office Online', OC.PERMISSION_UPDATE, OC.imagePath('core', 'actions/play'), wopiViewer.onEditWord);
+			OCA.Files.fileActions.register('application/vnd.openxmlformats-officedocument.wordprocessingml.template', 'Edit in Office Online', OC.PERMISSION_UPDATE, OC.imagePath('core', 'actions/play'), wopiViewer.onEditWord);
+			//OCA.Files.fileActions.register('application/vnd.oasis.opendocument.text', 'Edit in Office Online', OC.PERMISSION_UPDATE, OC.imagePath('core', 'actions/play'), wopiViewer.onEditWord);
 			OCA.Files.fileActions.register('application/vnd.ms-powerpoint', 'Edit in Office Online', OC.PERMISSION_UPDATE, OC.imagePath('core', 'actions/play'), wopiViewer.onEditPowerpoint);
+			OCA.Files.fileActions.register('application/vnd.ms-powerpoint.addin.macroEnabled.12', 'Edit in Office Online', OC.PERMISSION_UPDATE, OC.imagePath('core', 'actions/play'), wopiViewer.onEditPowerpoint);
+			OCA.Files.fileActions.register('application/vnd.ms-powerpoint.presentation.macroEnabled.12', 'Edit in Office Online', OC.PERMISSION_UPDATE, OC.imagePath('core', 'actions/play'), wopiViewer.onEditPowerpoint);
+			OCA.Files.fileActions.register('application/vnd.ms-powerpoint.slideshow.macroEnabled.12', 'Edit in Office Online', OC.PERMISSION_UPDATE, OC.imagePath('core', 'actions/play'), wopiViewer.onEditPowerpoint);
+			OCA.Files.fileActions.register('application/vnd.ms-powerpoint.template.macroEnabled.12', 'Edit in Office Online', OC.PERMISSION_UPDATE, OC.imagePath('core', 'actions/play'), wopiViewer.onEditPowerpoint);
+			OCA.Files.fileActions.register('application/vnd.openxmlformats-officedocument.presentationml.presentation', 'Edit in Office Online', OC.PERMISSION_UPDATE, OC.imagePath('core', 'actions/play'), wopiViewer.onEditPowerpoint);
+			OCA.Files.fileActions.register('application/vnd.openxmlformats-officedocument.presentationml.template', 'Edit in Office Online', OC.PERMISSION_UPDATE, OC.imagePath('core', 'actions/play'), wopiViewer.onEditPowerpoint);
+			OCA.Files.fileActions.register('application/vnd.openxmlformats-officedocument.presentationml.slideshow', 'Edit in Office Online', OC.PERMISSION_UPDATE, OC.imagePath('core', 'actions/play'), wopiViewer.onEditPowerpoint);
+			//OCA.Files.fileActions.register('application/vnd.oasis.opendocument.presentation', 'Edit in Office Online', OC.PERMISSION_UPDATE, OC.imagePath('core', 'actions/play'), wopiViewer.onEditPowerpoint);
 			OCA.Files.fileActions.register('application/vnd.ms-excel', 'Edit in Office Online', OC.PERMISSION_UPDATE, OC.imagePath('core', 'actions/play'), wopiViewer.onEditExcel);
+			OCA.Files.fileActions.register('application/vnd.ms-excel.addin.macroEnabled.12', 'Edit in Office Online', OC.PERMISSION_UPDATE, OC.imagePath('core', 'actions/play'), wopiViewer.onEditExcel);
+			OCA.Files.fileActions.register('application/vnd.ms-excel.sheet.binary.macroEnabled.12', 'Edit in Office Online', OC.PERMISSION_UPDATE, OC.imagePath('core', 'actions/play'), wopiViewer.onEditExcel);
+			OCA.Files.fileActions.register('application/vnd.ms-excel.sheet.macroEnabled.12', 'Edit in Office Online', OC.PERMISSION_UPDATE, OC.imagePath('core', 'actions/play'), wopiViewer.onEditExcel);
+			OCA.Files.fileActions.register('application/vnd.ms-excel.template.macroEnabled.12', 'Edit in Office Online', OC.PERMISSION_UPDATE, OC.imagePath('core', 'actions/play'), wopiViewer.onEditExcel);
+			OCA.Files.fileActions.register('application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'Edit in Office Online', OC.PERMISSION_UPDATE, OC.imagePath('core', 'actions/play'), wopiViewer.onEditExcel);
+			OCA.Files.fileActions.register('application/vnd.openxmlformats-officedocument.spreadsheetml.template', 'Edit in Office Online', OC.PERMISSION_UPDATE, OC.imagePath('core', 'actions/play'), wopiViewer.onEditExcel);
+			//OCA.Files.fileActions.register('application/vnd.oasis.opendocument.spreadsheet', 'Edit in Office Online', OC.PERMISSION_UPDATE, OC.imagePath('core', 'actions/play'), wopiViewer.onEditExcel);
+			//OCA.Files.fileActions.register('application/msonenote', 'Edit in Office Online', OC.PERMISSION_UPDATE, OC.imagePath('core', 'actions/play'), wopiViewer.onEditOnenote);
+
 
 			OCA.Files.fileActions.register('application/msword', 'Default View', OC.PERMISSION_READ, OC.imagePath('core', 'actions/play'), wopiViewer.onViewWord);
+			OCA.Files.fileActions.register('application/vnd.ms-word.document.macroEnabled.12', 'Default View', OC.PERMISSION_READ, OC.imagePath('core', 'actions/play'), wopiViewer.onViewWord); 
+			OCA.Files.fileActions.register('application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'Default View', OC.PERMISSION_READ, OC.imagePath('core', 'actions/play'), wopiViewer.onViewWord);
+			OCA.Files.fileActions.register('application/vnd.openxmlformats-officedocument.wordprocessingml.template', 'Default View', OC.PERMISSION_READ, OC.imagePath('core', 'actions/play'), wopiViewer.onViewWord); 
+			//OCA.Files.fileActions.register('application/vnd.oasis.opendocument.text', 'Default View', OC.PERMISSION_READ, OC.imagePath('core', 'actions/play'), wopiViewer.onViewWord);
 			OCA.Files.fileActions.register('application/vnd.ms-powerpoint', 'Default View', OC.PERMISSION_READ, OC.imagePath('core', 'actions/play'), wopiViewer.onViewPowerpoint);
+			OCA.Files.fileActions.register('application/vnd.ms-powerpoint.addin.macroEnabled.12', 'Default View', OC.PERMISSION_READ, OC.imagePath('core', 'actions/play'), wopiViewer.onViewPowerpoint);
+			OCA.Files.fileActions.register('application/vnd.ms-powerpoint.presentation.macroEnabled.12', 'Default View', OC.PERMISSION_READ, OC.imagePath('core', 'actions/play'), wopiViewer.onViewPowerpoint);
+			OCA.Files.fileActions.register('application/vnd.ms-powerpoint.slideshow.macroEnabled.12', 'Default View', OC.PERMISSION_READ, OC.imagePath('core', 'actions/play'), wopiViewer.onViewPowerpoint);
+			OCA.Files.fileActions.register('application/vnd.ms-powerpoint.template.macroEnabled.12', 'Default View', OC.PERMISSION_READ, OC.imagePath('core', 'actions/play'), wopiViewer.onViewPowerpoint);
+			OCA.Files.fileActions.register('application/vnd.openxmlformats-officedocument.presentationml.presentation', 'Default View', OC.PERMISSION_READ, OC.imagePath('core', 'actions/play'), wopiViewer.onViewPowerpoint);
+			OCA.Files.fileActions.register('application/vnd.openxmlformats-officedocument.presentationml.template', 'Default View', OC.PERMISSION_READ, OC.imagePath('core', 'actions/play'), wopiViewer.onViewPowerpoint);
+			OCA.Files.fileActions.register('application/vnd.openxmlformats-officedocument.presentationml.slideshow', 'Default View', OC.PERMISSION_READ, OC.imagePath('core', 'actions/play'), wopiViewer.onViewPowerpoint);
+			//OCA.Files.fileActions.register('application/vnd.oasis.opendocument.presentation', 'Default View', OC.PERMISSION_READ, OC.imagePath('core', 'actions/play'), wopiViewer.onViewPowerpoint);
 			OCA.Files.fileActions.register('application/vnd.ms-excel', 'Default View', OC.PERMISSION_READ, OC.imagePath('core', 'actions/play'), wopiViewer.onViewExcel);
+			OCA.Files.fileActions.register('application/vnd.ms-excel.addin.macroEnabled.12', 'Default View', OC.PERMISSION_READ, OC.imagePath('core', 'actions/play'), wopiViewer.onViewExcel);
+			OCA.Files.fileActions.register('application/vnd.ms-excel.sheet.binary.macroEnabled.12', 'Default View', OC.PERMISSION_READ, OC.imagePath('core', 'actions/play'), wopiViewer.onViewExcel);
+			OCA.Files.fileActions.register('application/vnd.ms-excel.sheet.macroEnabled.12', 'Default View', OC.PERMISSION_READ, OC.imagePath('core', 'actions/play'), wopiViewer.onViewExcel);
+			OCA.Files.fileActions.register('application/vnd.ms-excel.template.macroEnabled.12', 'Default View', OC.PERMISSION_READ, OC.imagePath('core', 'actions/play'), wopiViewer.onViewExcel);
+			OCA.Files.fileActions.register('application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'Default View', OC.PERMISSION_READ, OC.imagePath('core', 'actions/play'), wopiViewer.onViewExcel);
+			OCA.Files.fileActions.register('application/vnd.openxmlformats-officedocument.spreadsheetml.template', 'Default View', OC.PERMISSION_READ, OC.imagePath('core', 'actions/play'), wopiViewer.onViewExcel);
+			//OCA.Files.fileActions.register('application/vnd.oasis.opendocument.spreadsheet', 'Default View', OC.PERMISSION_READ, OC.imagePath('core', 'actions/play'), wopiViewer.onViewExcel);
+			//OCA.Files.fileActions.register('application/msonenote', 'Default View', OC.PERMISSION_READ, OC.imagePath('core', 'actions/play'), wopiViewer.onViewOnenote);
+
 
 			OCA.Files.fileActions.setDefault('application/msword', 'Default View');
+			OCA.Files.fileActions.setDefault('application/vnd.ms-word.document.macroEnabled.12', 'Default View');
+			OCA.Files.fileActions.setDefault('application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'Default View');
+			OCA.Files.fileActions.setDefault('application/vnd.openxmlformats-officedocument.wordprocessingml.template', 'Default View');
+			//OCA.Files.fileActions.setDefault('application/vnd.oasis.opendocument.text', 'Default View');
 			OCA.Files.fileActions.setDefault('application/vnd.ms-powerpoint', 'Default View');
+			OCA.Files.fileActions.setDefault('application/vnd.ms-powerpoint.addin.macroEnabled.12', 'Default View');
+			OCA.Files.fileActions.setDefault('application/vnd.ms-powerpoint.presentation.macroEnabled.12', 'Default View');
+			OCA.Files.fileActions.setDefault('application/vnd.ms-powerpoint.slideshow.macroEnabled.12', 'Default View');
+			OCA.Files.fileActions.setDefault('application/vnd.ms-powerpoint.template.macroEnabled.12', 'Default View');
+			OCA.Files.fileActions.setDefault('application/vnd.openxmlformats-officedocument.presentationml.presentation', 'Default View');
+			OCA.Files.fileActions.setDefault('application/vnd.openxmlformats-officedocument.presentationml.template', 'Default View');
+			OCA.Files.fileActions.setDefault('application/vnd.openxmlformats-officedocument.presentationml.slideshow', 'Default View');
+			//OCA.Files.fileActions.setDefault('application/vnd.oasis.opendocument.presentation', 'Default View');
 			OCA.Files.fileActions.setDefault('application/vnd.ms-excel', 'Default View');
+			OCA.Files.fileActions.setDefault('application/vnd.ms-excel.addin.macroEnabled.12', 'Default View');
+			OCA.Files.fileActions.setDefault('application/vnd.ms-excel.sheet.binary.macroEnabled.12', 'Default View');
+			OCA.Files.fileActions.setDefault('application/vnd.ms-excel.sheet.macroEnabled.12', 'Default View');
+			OCA.Files.fileActions.setDefault('application/vnd.ms-excel.template.macroEnabled.12', 'Default View');
+			OCA.Files.fileActions.setDefault('application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'Default View');
+			OCA.Files.fileActions.setDefault('application/vnd.openxmlformats-officedocument.spreadsheetml.template', 'Default View');
+			//OCA.Files.fileActions.setDefault('application/vnd.oasis.opendocument.spreadsheet', 'Default View');
+			//OCA.Files.fileActions.setDefault('application/msonenote', 'Default View');
 		}
 	});
 
