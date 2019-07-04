c5263ab30a6d54f4ea17584db6279b6501cb4dae
Diogo Castro <diogo.castro@cern.ch>
2019-07-04 18:52:16 +0200

    remove empty alert

--- js/script.js
+++ js/script.js
@@ -122,7 +122,7 @@
 				//$("header div#header").append(closeButton);
 				//$("header div#header #office_close_button").click(closeDocument);
 			} else {
-				alert(response.error);
+				console.error(response.error);
 			}
 		});
 	};
@@ -180,7 +180,7 @@
 					var viewerURL = wordViewer + encodeURI(response.wopi_src);
 					setView(viewerURL, response.wopi_src, token);
 				} else {
-					alert(response.error);
+					console.error(response.error);
 				}
 			});
 		},
@@ -222,7 +222,8 @@
 					window.location.hash = 'office';
 					var viewerURL = powerpointViewer + encodeURI(response.wopi_src);
 					setView(viewerURL, response.wopi_src, token);
-					alert(response.error);
+				} else {
+					console.error(response.error);
 				}
 			});
 		},
@@ -265,7 +266,7 @@
 					var viewerURL = excelViewer + encodeURI(response.wopi_src);
 					setView(viewerURL, response.wopi_src, token);
 				} else {
-					alert(response.error);
+					console.error(response.error);
 				}
 			});
 		},
